26506b8d4c4d5d9301d752306806f9e1f061d6a2
segzy2baba <segzy2baba1@gmail.com>
2025-05-03 00:04:19 +0100

    Refactor testimonials section into a reusable file and update service and index pages to include it

--- service.php
+++ service.php
@@ -265,57 +265,8 @@
     <!-- Featured Products End -->
 
 
-    <!-- Testimonial Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                <p class="fw-medium text-uppercase text-primary mb-2">Testimonials</p>
-                <h1 class="display-5 mb-5">What Our Customers Say</h1>
-            </div>
-            <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
-                <div class="testimonial-item text-center">
-                    <div class="testimonial-img position-relative">
-                        <img class="img-fluid rounded-circle mx-auto mb-5" src="img/testimonial-1.jpg">
-                        <div class="btn-square bg-primary rounded-circle">
-                            <i class="fa fa-quote-left text-white"></i>
-                        </div>
-                    </div>
-                    <div class="testimonial-text text-center rounded p-4">
-                        <p>The Natraj spanners we ordered have proven to be exceptionally durable. After using them daily in our workshop for over a year, they still perform like new. The quality is unmatched and the wholesale pricing was excellent.</p>
-                        <h5 class="mb-1">Michael Johnson</h5>
-                        <span class="fst-italic">Construction Supervisor</span>
-                    </div>
-                </div>
-                <div class="testimonial-item text-center">
-                    <div class="testimonial-img position-relative">
-                        <img class="img-fluid rounded-circle mx-auto mb-5" src="img/testimonial-2.jpg">
-                        <div class="btn-square bg-primary rounded-circle">
-                            <i class="fa fa-quote-left text-white"></i>
-                        </div>
-                    </div>
-                    <div class="testimonial-text text-center rounded p-4">
-                        <p>As a hardware store owner, I've been purchasing tools from this distributor for over 5 years. Their Natraj files are consistently our best-selling item due to their quality and reliability. The wholesale service is always prompt and efficient.</p>
-                        <h5 class="mb-1">Sarah Williams</h5>
-                        <span class="fst-italic">Hardware Store Owner</span>
-                    </div>
-                </div>
-                <div class="testimonial-item text-center">
-                    <div class="testimonial-img position-relative">
-                        <img class="img-fluid rounded-circle mx-auto mb-5" src="img/testimonial-3.jpg">
-                        <div class="btn-square bg-primary rounded-circle">
-                            <i class="fa fa-quote-left text-white"></i>
-                        </div>
-                    </div>
-                    <div class="testimonial-text text-center rounded p-4">
-                        <p>We equipped our entire maintenance team with their complete set of building tools. The quality is exceptional, especially the Natraj products. Their equipment rental service also saved us significant costs on our latest project.</p>
-                        <h5 class="mb-1">Robert Ondieki</h5>
-                        <span class="fst-italic">Facility Manager</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Testimonial End -->
+    <!-- Include Testimonials -->
+    <?php include 'testimonials.php'; ?>
 
 
     <!-- Footer Start -->
